4983083b745e893875f65347782589ba1db0b128
Sumaiya Sinja <sumiyasinja@gmail.com>
2023-10-18 13:07:52 +0600

    clent side app initialized

---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const Root = () => {
+    return (
+        <div>
+            Rootsyaaa  
+            <Outlet />
+        </div>
+    );
+};
+
+export default Root;
